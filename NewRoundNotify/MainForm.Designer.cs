3e85e8833d2c61facb9a43536b8f7d183db505e3
Moritz Uehling <git@moritzuehling.de>
2015-12-09 20:09:04 +0100

    Initial commit. Already wrote CS:GO discovery.

---
+++ NewRoundNotify/MainForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace NewRoundNotify
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.generateConfigButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.startServerButton = new System.Windows.Forms.Button();
+            this.minimizeButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.generateConfigButton);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 51);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Status";
+            // 
+            // generateConfigButton
+            // 
+            this.generateConfigButton.Location = new System.Drawing.Point(103, 15);
+            this.generateConfigButton.Name = "generateConfigButton";
+            this.generateConfigButton.Size = new System.Drawing.Size(119, 23);
+            this.generateConfigButton.TabIndex = 1;
+            this.generateConfigButton.Text = "Generate!";
+            this.generateConfigButton.UseVisualStyleBackColor = true;
+            this.generateConfigButton.Click += new System.EventHandler(this.generateConfigButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CS:GO Config";
+            // 
+            // startServerButton
+            // 
+            this.startServerButton.Location = new System.Drawing.Point(13, 71);
+            this.startServerButton.Name = "startServerButton";
+            this.startServerButton.Size = new System.Drawing.Size(97, 23);
+            this.startServerButton.TabIndex = 1;
+            this.startServerButton.Text = "Start Server";
+            this.startServerButton.UseVisualStyleBackColor = true;
+            // 
+            // minimizeButton
+            // 
+            this.minimizeButton.Location = new System.Drawing.Point(138, 71);
+            this.minimizeButton.Name = "minimizeButton";
+            this.minimizeButton.Size = new System.Drawing.Size(97, 23);
+            this.minimizeButton.TabIndex = 2;
+            this.minimizeButton.Text = "Minimize to tray";
+            this.minimizeButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 108);
+            this.Controls.Add(this.minimizeButton);
+            this.Controls.Add(this.startServerButton);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "StatsHelix CS:GO Round Notifier";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button generateConfigButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button startServerButton;
+        private System.Windows.Forms.Button minimizeButton;
+    }
+}
+
---
+++ NewRoundNotify/MainForm.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewRoundNotify
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            GetCSGODir();
+        }
+
+        public void UpdateStatus()
+        {
+            
+        }
+
+        private void generateConfigButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Returns the location of the CS:GO installation, or null if it's unable to find it. 
+        /// </summary>
+        /// <returns></returns>
+        private string GetCSGODir()
+        {
+            string steamPath = (string)Registry.GetValue("HKEY_CURRENT_USER\\Software\\Valve\\Steam", "SteamPath", "");
+
+            string pathsFile = Path.Combine(steamPath, "steamapps", "libraryfolders.vdf");
+
+            if (!File.Exists(pathsFile))
+                return null;
+
+            List<string> libraries = new List<string>();
+            libraries.Add(Path.Combine(steamPath));
+
+            var pathVDF = File.ReadAllLines(pathsFile);
+
+
+            // Okay, this is not a full vdf-parser, but it seems to work pretty much, since the 
+            // vdf-grammar is pretty easy. Hopefully it never breaks. I'm too lazy to write a full vdf-parser though. 
+            Regex pathRegex = new Regex(@"\""(([^\""]*):\\([^\""]*))\""");
+            foreach (var line in pathVDF)
+            {
+                if(pathRegex.IsMatch(line))
+                {
+                    string match = pathRegex.Matches(line)[0].Groups[1].Value;
+
+                    // De-Escape vdf. 
+                    libraries.Add(match.Replace("\\\\", "\\"));
+                }
+            }
+
+            foreach(var library in libraries)
+            {
+                string csgoPath = Path.Combine(library, "steamapps\\common\\Counter-Strike Global Offensive");
+                if (!Directory.Exists(csgoPath))
+                {
+                    return csgoPath;
+                }
+            }
+
+
+            return null;
+        }
+    }
+}
